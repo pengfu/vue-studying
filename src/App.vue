5f50cf91bf941288819668cc924f27bd2db1f98f
chang_su <chang_su@hunliji.com>
2016-12-14 14:48:39 +0800

    moment的使用

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <button @click="click1">
+      showMoment
+    </button>
+    <span v-if="visible1">{{result1}}</span>
+
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
 
+import moment from 'moment'
 export default {
   name: 'app',
+  data(){
+    return {
+      visible1 : false
+    }
+  },
+  computed:{
+    result1(){
+      return moment().format('YYYY年M月DD日HH时mm分ss秒')
+    }
+  },
+  methods:{
+    click1(){
+      this.visible1 = true
+    }
+  },
   components: {
-    Hello
+
   }
 }
 </script>
